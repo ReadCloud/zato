d921096da07d6f103fe94b50d8e3b91c9a814701
Daniel Manning <dmisdm@users.noreply.github.com>
2024-04-11 05:43:36 +0000

    Attempt at declaring dependencies for zato-server

--- code/zato-server/setup.py
+++ code/zato-server/setup.py
@@ -8,8 +8,13 @@ Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 
 # flake8: noqa
 from setuptools import setup, find_packages
-
+import os
 version = '3.2'
+def parse_requirements(requirements):
+    ignored = ['#', 'setuptools', '-e']
+
+    with open(requirements) as f:
+        return [line.split('==')[0] for line in f if line.strip() and not any(line.startswith(prefix) for prefix in ignored)]
 
 setup(
       name = 'zato-server',
@@ -23,6 +28,8 @@ setup(
       packages = find_packages('src'),
 
       namespace_packages = ['zato'],
+      install_requires = parse_requirements(
+          os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')),
 
       zip_safe = False,
 )
